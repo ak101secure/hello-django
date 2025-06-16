8b2569c8009928478162b6c1410e221083bcab0e
afzal <afzal@gmail.com>
2025-06-16 18:45:24 +0600

    add new view hello chadni

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world , hello_chadni 
 
 urlpatterns = [
     path('', hello_world),
+    path('chadni/',hello_chadni)
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World! everything is ok ! Thanks a lot ")
+
+def hello_chadni(request):
+    return HttpResponse("Hello, Chadni! everything is ok ! Thanks a lot ")
